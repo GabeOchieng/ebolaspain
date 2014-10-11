ee1671d8fd6142c3a5a469138bd730b45450f1ee
Jose Angel <alvarezja@gmail.com>
2014-10-11 21:55:50 +0200

    dimisiones

--- src/sendV2.py
+++ src/sendV2.py
@@ -2,6 +2,8 @@ from twython import Twython, TwythonError
 import datetime
 import os
 
+max_news = 1
+
 folder = os.path.dirname(os.path.realpath(__file__))
 j = datetime.datetime.now()
 
@@ -15,10 +17,8 @@ st = ''
 # Requires Authentication as of Twitter API v1.1
 twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
-for i in range(1,3):
-    print i
+for i in range(1,max_news+1):
     p = os.path.join(folder,'db'+str(i)+'.txt')
-    print p
     if os.path.exists(p):       
         fd = open(p,'r')
         st = fd.read() + "\n" + j.strftime('%d%b %H:%Mh') + " #ebola"
